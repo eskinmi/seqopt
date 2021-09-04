62897edf4a71f2d5f351b6416a2b6e55f6ec2e09
mustafasamedeskin <meskin@wehkamp.nl>
2021-09-04 14:41:01 +0200

    added init to model and optimizer

--- seqopt/__init__.py
+++ seqopt/__init__.py
@@ -1,7 +1,4 @@
-#
-# from seqopt.optimizers import optimizers
-# from seqopt import callbacks
-# from seqopt.models import utils
-# from seqopt.models.model import OptModel
-#
-#
+from seqopt.optimizers import optimizers
+from seqopt import callbacks
+from seqopt.models import utils
+from seqopt.models.model import OptModel
